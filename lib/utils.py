d943a8b02aa49e9adaeccdfd0c5b19c0e5718f10
tarithj <tarithj@gmail.com>
2021-04-22 20:04:27 +0530

    created utils.cursor_to_list

--- lib/utils.py
+++ lib/utils.py
@@ -9,3 +9,19 @@ def format_todo(all_todos: pymongo.collection.Cursor) -> str:
         todo_list += f"\n**{i}.** {x['title']} - `{x['deadline']}`"
         i += 1
     return todo_list
+
+
+def cursor_to_list(cursor: pymongo.collection.Cursor, properties: [str]):
+    """
+    converts cursor to list.
+
+    :parameter properties: should be a array of string indexes
+    """
+
+    v = []
+    for x in cursor:
+        v2 = []
+        for c_property in properties:
+            v2.append(x[c_property])
+
+        v.append(v2)
