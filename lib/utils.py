85dca7f7427cab8974c41f9f263041f5efa662d8
tarithj <tarithj@gmail.com>
2021-04-22 19:42:46 +0530

    created utils.format_todo function

---
+++ lib/utils.py
@@ -0,0 +1,11 @@
+import pymongo
+
+
+def format_todo(all_todos: [pymongo.collection.Cursor]) -> str:
+    todo_list = ""
+    i = 1
+    # Format the todos for discord
+    for x in all_todos:
+        todo_list += f"\n**{i}.** {x['title']} - `{x['deadline']}`"
+        i += 1
+    return todo_list
