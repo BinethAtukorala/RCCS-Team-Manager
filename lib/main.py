27b9355de890385c1c73669cad25ded090142609
Bineth Atukorala <bineth.mandiv@gmail.com>
2021-05-06 12:05:07 +0530

    Finished functionality to add Todos

--- lib/main.py
+++ lib/main.py
@@ -7,6 +7,8 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 import utils
+import asyncio
+from datetime import datetime
 
 # -------- MongoDB inits
 myClient = MongoClient('mongodb://localhost:27017')
@@ -27,7 +29,7 @@ TOKEN = ""
 with open("token", "r") as file:
     TOKEN = file.read()
 
-bot = commands.Bot(command_prefix="&")
+bot = commands.Bot(command_prefix="~")
 
 # =========== Commands ==============
 
@@ -40,7 +42,7 @@ async def listTodos(ctx):
     if(len(allTodos) > 0):
 
         # Format the todos for discord
-        todoList = utils.format_todo(allTodos)
+        todoList = utils.format_todolist(allTodos)
 
         response = "**All upcoming todos in this server** {0}\n{1}".format(ctx.message.author.mention, todoList)
     else:
@@ -50,37 +52,209 @@ async def listTodos(ctx):
 
 # List todos assigned and created by the message author
 @bot.command(name='list')
-async def listTodos(ctx):
+async def listTodos(ctx, index=0):
     creator = ctx.message.author.id
 
     # Todos assigned to the author
     allTodos = utils.cursor_to_list(todoCol.find({"members": creator}))
 
-    # Check if any exist
-    if(len(allTodos) > 0):
+    if(index > 0):
+        response = allTodos[index]
 
-        # Format for discord
-        todoList = utils.format_todo(allTodos)
-
-        response = "**Your upcoming todos** {0}\n{1}".format(ctx.message.author.mention, todoList)
     else:
-        response = "No upcoming todos for you. Yayy"
+        # Check if any exist
+        if(len(allTodos) > 0):
 
-    # Todos created by author
-    allTodos = utils.cursor_to_list(todoCol.find({"creator": creator}))
+            # Format for discord
+            todoList = utils.format_todolist(allTodos)
 
-    # Check if any exist
-    if(len(allTodos) > 0):
+            response = "**Your upcoming todos**\n{0}".format(todoList)
+        else:
+            response = "No upcoming todos for you. Yayy"
 
-        # Format for discord
-        todoList = utils.format_todo(allTodos)
+        # Todos created by author
+        allTodos = utils.cursor_to_list(todoCol.find({"creator": creator}))
 
-        response+= "\n\n**Todos created by you** {0}\n{1}".format(ctx.message.author.mention, todoList)
+        # Check if any exista
+        if(len(allTodos) > 0):
 
+            # Format for discord
+            todoList = utils.format_todolist(allTodos)
 
-    
+            response+= "\n\n**Todos created by you** {0}\n{1}".format(ctx.message.author.mention, todoList)
+
 
     await ctx.send(response)
+
+# Create new todo
+@bot.command(name='add')
+async def addTodo(ctx, *members_str):
+    creator = ctx.message.author.id
+    
+    await ctx.send("Type `cancel` to exit and `skip` to skip optional fields.\n\n:grey_question: What is the title of the todo?")
+
+    # Check the whether the reply is given by the author of the command
+    def check(m):
+        return m.author == ctx.message.author
+    
+    # Wait for reply and exit if it's 'cancel'
+    # !!!! Code is too redundant
+    try:
+        reply = (await bot.wait_for('message', check=check, timeout=60.0)).content
+        if(reply.lower() == "cancel"):
+            return
+        title = reply
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you took too long to reply")
+        return
+
+    await ctx.send(f":white_check_mark: Title is `{title}`\n\n:grey_question: Please give me a brief description about it (optional).")
+
+    # Wait for reply and exit if it's 'cancel'
+    try:
+        reply = (await bot.wait_for('message', check=check, timeout=60.0)).content
+        if(reply.lower() == "cancel"):
+            return
+        if(reply.lower() == "skip"):
+            description = ""
+        else:
+            description = reply
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you took too long to reply")
+        return
+
+    await ctx.send(f":white_check_mark: Description is `{description}`\n\n:grey_question: What project does this relate to? (optional)")
+
+    # Wait for reply and exit if it's 'cancel'
+    try:
+        reply = (await bot.wait_for('message', check=check, timeout=60.0)).content
+        if(reply.lower() == "cancel"):
+            return
+        if(reply.lower() == "skip"):
+            project = ""
+        else:
+            project = reply
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you took too long to reply")
+        return
+
+    await ctx.send(f":white_check_mark: Project is `{project}`\n\n:grey_question: When is the deadline? (DD/MM/YYYY)")
+
+    def check_date(m):
+        return check(m) and utils.is_a_date(m.content)
+
+    # Wait for reply and exit if it's 'cancel'
+    try:
+        reply = (await bot.wait_for('message', check=check_date, timeout=60.0)).content
+        if(reply.lower() == "cancel"):
+            return
+        deadline = datetime.strptime(reply, "%d/%m/%Y")
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you took too long to reply")
+
+    await ctx.send(f":white_check_mark: Deadline is `{deadline.strftime('%d/%m/%Y')}`\n\n:grey_question: Please mention the people you want to assign this to. (optional)")
+    members = []
+    # Wait for reply and exit if it's 'cancel'
+    try:
+        reply = (await bot.wait_for('message', check=check, timeout=60.0)).content
+        if(reply.lower() == "cancel"):
+            return
+        if(reply.lower() != "skip"):
+            for x in reply.split():
+                members.append(await bot.fetch_user(x[3:-1]))
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you took too long to reply")
+
+    members_text = ""
+    for x in members:
+        members_text+=f"{x}, "
+    members_text = members_text[:-2]
+
+    await ctx.send(f":white_check_mark: Members are `{members_text}`\n\n:grey_question: Please list out subtasks of the todo in each line. (optional)")
+    subtasks = []
+    # Wait for reply and exit if it's 'cancel'
+    try:
+        reply = (await bot.wait_for('message', check=check, timeout=60.0)).content
+        if(reply.lower() == "cancel"):
+            return
+        if(reply.lower() != "skip"):
+            for x in reply.split("\n"):
+                subtasks.append(x)
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you took too long to reply")
+    
+    subtasks_text = ""
+    for x in subtasks:
+        subtasks_text+=f"{x}, "
+    subtasks_text = subtasks_text[:-2]
+
+    messageSent = await ctx.send(f"""Are the following details correct?
+
+:white_small_square: Title - {title}
+:white_small_square: Description - {description}
+:white_small_square: Project - {project}
+:white_small_square: Deadline - {deadline.strftime("%d/%m/%Y")}
+:white_small_square: Members - {members_text}
+:white_small_square: Subtasks - {subtasks_text}
+""")
+
+    await messageSent.add_reaction("✅")
+    await messageSent.add_reaction("❌")
+
+    def checkReaction(reaction, user):
+        return user == ctx.message.author and (str(reaction.emoji) == "✅" or str(reaction.emoji) == "❌")
+    
+    try:
+        reaction, user = await bot.wait_for('reaction_add', check=checkReaction, timeout=20)
+    except asyncio.TimeoutError:
+        await ctx.send("Sorry, you took too long to react.")
+    
+    if(str(reaction.emoji) == "✅"):
+        await ctx.send(":white_check_mark: Done, Todo Created")
+
+        subtasksDictArray = []
+        for x in subtasks:
+            subtasksDictArray.append({"title":x, "completed": False})
+        membersDictArray = []
+        for x in members:
+            membersDictArray.append(x.id)
+
+        newTodoDocument = {
+            "title": title,
+            "description": description,
+            "project": project,
+            "deadline": deadline,
+            "creator": creator,
+            "members": membersDictArray,
+            "subtasks": subtasksDictArray
+        }
+
+        todoCol.insert_one(newTodoDocument)
+
+        print(f"Todo confirmed: {newTodoDocument}")
+    else:
+        await ctx.send("Todo Rejected. :x:")
+        print("Todo rejected")
+
+    # response = "You mentioned,"
+    # members = []
+    # if(len(members_str) > 0):
+    #     for member in members_str:
+    #         members.append(await bot.fetch_user(member[3:-1]))
+    #     for member in members:
+    #         response += f"\n{member.id}"
+    # else:
+    #     response += " no one."
+    # await ctx.send(response)
+
+# Test a feature
+@bot.command(name="test")
+async def testing(ctx):
+    await ctx.send("Please mention some people")
+    def check(reaction, user):
+        return user == ctx.message.author
+    reaction, user = await bot.wait_for('reaction_add', check=check, timeout=20)
+    print(str(reaction))
 
 # Call someone (For fun)
 @bot.command(name="call")
@@ -92,4 +266,5 @@ async def call(ctx, user: discord.Member = None):
 
 # -------- Run the goddamn bot
 
+print("## Starting Bot ##")
 bot.run(TOKEN)
--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,12 @@
+name = "utils"
+
 import pymongo
+import re
+from datetime import datetime
 
 
-def format_todo(all_todos: list) -> str:
+
+def format_todolist(all_todos: list) -> str:
     todo_list = ""
     i = 1
     # Format the todos for discord
@@ -21,3 +26,16 @@ def cursor_to_list(cursor: pymongo.collection.Cursor):
     for x in cursor:
         v.append(x)
     return v
+
+def is_a_date(string: str) -> bool:
+
+    date_re = re.compile(r"\d\d/\d\d/\d\d\d\d")
+    if(date_re.match(string) != None):
+        try:
+            date = deadline = datetime.strptime(string, "%d/%m/%Y")
+        except ValueError:
+            return False
+        else:
+            return True
+    return False
+
