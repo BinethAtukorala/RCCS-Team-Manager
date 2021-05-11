bc1bf6f39bfc57997f160ac7e567a27afd60a85c
Tarith <tarithj@gmail.com>
2021-05-11 14:54:04 +0530

    created GetAssignedTODO function in db_utils package

--- db_utils/main.go
+++ db_utils/main.go
@@ -2,9 +2,14 @@ package db_utils
 
 import (
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"rccs_team_manager/models"
 )
 
 func AddTodo(todo models.TODO, database *mgo.Database) error {
 	return database.C("todo").Insert(&todo)
+}
+
+func GetAssignedTODO(member models.Member, database mgo.Database) *mgo.Query {
+	return database.C("todo").Find(bson.M{"_discordid": member.DiscordId})
 }
