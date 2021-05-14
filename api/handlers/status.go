fd89fe8c1e77b9d5846f0e8ede79cc88549aa2e3
Tarith <tarithj@gmail.com>
2021-05-14 13:39:56 +0530

    created new api endpoint /api/status

--- api/handlers/status.go
+++ api/handlers/status.go
@@ -2,11 +2,8 @@ package handlers
 
 import (
 	"fmt"
-	"github.com/op/go-logging"
 	"net/http"
 )
-
-var log = logging.MustGetLogger("handlers")
 
 func ProvideStatusHandler(getStatus func() string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
