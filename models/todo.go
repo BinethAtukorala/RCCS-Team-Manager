e294d0d17dcc9016c1b90e36e4dd72220bb3fdf2
Tarith <tarithj@gmail.com>
2021-05-14 21:04:40 +0530

    renamed TODO.Project type from Project to string

--- models/todo.go
+++ models/todo.go
@@ -5,7 +5,7 @@ import "time"
 type TODO struct {
 	Title       string    `bson:"_title"`
 	Description string    `bson:"_description"`
-	Project     Project   `bson:"_project"`
+	Project     string    `bson:"_project"`
 	Deadline    time.Time `bson:"_deadline"`
 	Members     []Member  `bson:"_members"`
 	SubTasks    []Task    `bson:"_subtasks"`
