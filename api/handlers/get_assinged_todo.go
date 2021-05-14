28ddf84d7f8cd8a6e815f1a78a35e53ed71f3c39
Tarith <tarithj@gmail.com>
2021-05-14 13:39:27 +0530

    created new api endpoint /api/todo/get/assigned

---
+++ api/handlers/get_assinged_todo.go
@@ -0,0 +1,48 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"gopkg.in/mgo.v2"
+	"net/http"
+	"rccs_team_manager/db_utils"
+	"rccs_team_manager/models"
+)
+
+type getAssignedTodoRequest struct {
+	DiscordId string `bson:"discordid"`
+}
+
+func ProvideGetAssignedTodoHandler(database *mgo.Database) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var decodedTodo getAssignedTodoRequest
+
+		err := json.NewDecoder(r.Body).Decode(&decodedTodo)
+		if err != nil {
+			fmt.Fprintf(w, "Invalid format")
+			log.Infof("Invalid format: ", err)
+			return
+		}
+		println("a " + decodedTodo.DiscordId)
+		var return_todo []models.TODO
+
+		err = db_utils.GetAssignedTODO(models.Member{
+			Name:      "",
+			DiscordId: decodedTodo.DiscordId,
+		}, database).All(&return_todo)
+		if err != nil {
+			fmt.Fprintf(w, "DB error")
+			log.Infof("DB error: ", err)
+			return
+		}
+
+		marshaled_todos, err := json.Marshal(return_todo)
+		if err != nil {
+			fmt.Fprintf(w, "Cant marshal data")
+			log.Infof("Cant marshal data: ", err)
+			return
+		}
+		fmt.Fprintf(w, string(marshaled_todos))
+		log.Infof("Endpoint Hit: /api/todo/get/assigned")
+	}
+}
--- db_utils/main.go
+++ db_utils/main.go
@@ -10,6 +10,10 @@ func AddTodo(todo models.TODO, database *mgo.Database) error {
 	return database.C("todo").Insert(&todo)
 }
 
-func GetAssignedTODO(member models.Member, database mgo.Database) *mgo.Query {
-	return database.C("todo").Find(bson.M{"_discordid": member.DiscordId})
+func GetAssignedTODO(member models.Member, database *mgo.Database) *mgo.Query {
+	return database.C("todo").Find(bson.M{
+		"_members": bson.M{
+			"$elemMatch": bson.M{"_discordid": member.DiscordId},
+		},
+	})
 }
